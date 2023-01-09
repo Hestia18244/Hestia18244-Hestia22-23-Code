ea45eaa1e354d7ba4026f980d7deae63e886aea8
xavier <xavierwood3737@gmail.com>
2023-01-08 20:33:13 -0600

    Updating android studio backup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/HestiaAutonomousMoveLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/HestiaAutonomousMoveLeft.java
@@ -7,41 +7,41 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @Autonomous(name = "HestiaAutonomousMoveLeft")
 
 public class HestiaAutonomousMoveLeft extends LinearOpMode {
-                      //calls all the motors
+        //calls all the motors
         DcMotor frontLeftMotor;
         DcMotor frontRightMotor;
         DcMotor backLeftMotor;
         DcMotor backRightMotor;
         
         //all code listed after here happens when opmode is active.
-  public void runOpMode(){
-        waitForStart();
-        //maps the motors to what they are named on the drivers hub
-        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
-        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
-        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
-        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+        public void runOpMode(){
+            waitForStart();
+            //maps the motors to what they are named on the drivers hub
+            frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+            frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+            backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+            backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
   
-        //sets power to all the motors so that it moves left
-        frontRightMotor.setPower(-.5);
-        frontLeftMotor.setPower(-.5);
-        backRightMotor.setPower(.5);
-        backLeftMotor.setPower(.5);
-    //sleep causes this to happen for 1 seconds (1000 milliseconds)
-    sleep(1100);
-    //sets power to all the motors to move backwards a small amount
-      frontRightMotor.setPower(.4);
-      frontLeftMotor.setPower(-.4);
-      backRightMotor.setPower(.4);
-      backLeftMotor.setPower(-.4);
-      sleep(200);
-    //sets the power of the motors to zero
-    frontRightMotor.setPower(0);
-    frontLeftMotor.setPower(0);
-    backRightMotor.setPower(0);
-    backLeftMotor.setPower(0);
-    //causes this to happen for 29 seconds (which is the whole duration of autonomous)
-    sleep(29000);
-    telemetry.update();
+            //sets power to all the motors so that it moves left
+            frontRightMotor.setPower(-.5);
+            frontLeftMotor.setPower(-.5);
+            backRightMotor.setPower(.5);
+            backLeftMotor.setPower(.5);
+            //sleep causes this to happen for 1 seconds (1000 milliseconds)
+            sleep(1100);
+            //sets power to all the motors to move backwards a small amount
+            frontRightMotor.setPower(.4);
+            frontLeftMotor.setPower(-.4);
+            backRightMotor.setPower(.4);
+            backLeftMotor.setPower(-.4);
+            sleep(200);
+            //sets the power of the motors to zero
+            frontRightMotor.setPower(0);
+            frontLeftMotor.setPower(0);
+            backRightMotor.setPower(0);
+            backLeftMotor.setPower(0);
+            //causes this to happen for 29 seconds (which is the whole duration of autonomous)
+            sleep(29000);
+            telemetry.update();
      }
 }
